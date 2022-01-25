eb493ba08220163784c39ef548755dcbab215a73
Waddell02 <lukewaddell16@gmail.com>
2022-01-25 13:07:35 +0000

    Added Task1C

--- test_geofunctions.py
+++ test_geofunctions.py
@@ -1,4 +1,4 @@
-from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_by_distance, stations_within_radius
 from floodsystem.stationdata import build_station_list
 from haversine import haversine, Unit
 
@@ -12,4 +12,15 @@ def test_stations_by_distance():
     # Test if the function is working correctly
     assert test_distance_list[0][0].name == "Cambridge Jesus Lock"
 
-test_stations_by_distance()
+def test_stations_within_radius():
+    """Test building list of stations within radius"""
+    # Build a list of stations
+    stations = build_station_list()
+    # Define centre
+    centre = (52.2053, 0.1218)
+    test_radius_list = stations_within_radius(stations, centre, 10)
+    # Test that a list is built
+    assert len(test_radius_list) > 0
+    # Test that all stations are within the range
+    for n in range(len(stations)):
+        assert haversine(centre, test_radius_list[n].coord) < 10
