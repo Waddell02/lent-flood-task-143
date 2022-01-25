d233b99401f6488b2d48b62bf841d0ba46c8e8c9
Waddell02 <lukewaddell16@gmail.com>
2022-01-25 12:54:00 +0000

    1C completed without comments

--- Task1C.py
+++ Task1C.py
@@ -1,1 +1,16 @@
 from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+def run():
+    """Requirements for Task 1C """
+    stations = build_station_list()
+    centre = (52.2053, 0.1218)
+    range_list = stations_within_radius(stations, centre, 10)
+    modified_range_list = []
+    for station in range_list:
+        modified_range_list.append(station.name)
+    print(sorted(modified_range_list))
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
