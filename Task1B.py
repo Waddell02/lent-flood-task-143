c5d75df86608379448ab89acb694ece145cec24b
Waddell02 <lukewaddell16@gmail.com>
2022-01-25 12:19:40 +0000

    Added comments

--- Task1B.py
+++ Task1B.py
@@ -10,9 +10,12 @@ def run():
     p = (52.2053, 0.1218)
 
     full_distance_list = stations_by_distance(stations, p)
+    # Create empty list
     modified_list = []
+    # Add station names and towns
     for station, distance in full_distance_list:
         modified_list.append((station.name, station.town, distance))
+    # First and last 10 entries converted to strings and printed 
     print("The 10 closest stations are: " + str(modified_list[:10]))
     print("The 10 furthest stations are: " + str(modified_list[-10:]))
 
