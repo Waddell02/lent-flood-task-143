65467f20658d19c270f6ba2eb83f7833d01690dd
Waddell02 <lukewaddell16@gmail.com>
2022-01-25 21:19:58 +0000

    Took 'build station list' out of tests

--- test_geofunctions.py
+++ test_geofunctions.py
@@ -1,21 +1,21 @@
-from floodsystem.geo import stations_by_distance, stations_within_radius
+from floodsystem.geo import rivers_with_station, stations_by_distance, stations_within_radius, stations_by_river
 from floodsystem.stationdata import build_station_list
 from haversine import haversine, Unit
 
+# Build station list
+stations = build_station_list()
+
 def test_stations_by_distance():
     """Test building list of stations"""
-    # Build a list of stations
-    stations = build_station_list()
     test_distance_list = stations_by_distance(stations, (52.2053, 0.1218))
     # Test if the function is called
     assert len(test_distance_list) > 0
     # Test if the function is working correctly
     assert test_distance_list[0][0].name == "Cambridge Jesus Lock"
 
+
 def test_stations_within_radius():
     """Test building list of stations within radius"""
-    # Build a list of stations
-    stations = build_station_list()
     # Define centre
     centre = (52.2053, 0.1218)
     test_radius_list = stations_within_radius(stations, centre, 10)
@@ -24,3 +24,10 @@ def test_stations_within_radius():
     # Test that all stations are within the range
     for n in range(len(test_radius_list)):
         assert haversine(centre, test_radius_list[n].coord) < 10
+
+def test_rivers_with_station():
+    assert "River Cam" in rivers_with_station(stations)
+    assert type(rivers_with_station) == "set"
+
+def test_stations_by_river():
+    assert "Cambridge Jesus Lock" in stations_by_river(stations)["River Cam"]
