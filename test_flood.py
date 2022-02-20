1a869f822d6cdd01bb846bb216697398f6ec8d13
Waddell02 <lukewaddell16@gmail.com>
2022-02-20 11:31:06 +0000

    Added test for 2C

--- test_flood.py
+++ test_flood.py
@@ -1,5 +1,5 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
 
 def test_stations_level_over_threshold():
     stations = build_station_list()
@@ -7,3 +7,10 @@ def test_stations_level_over_threshold():
     assert stations_level_over_threshold(stations, 0.8)[0][1] > 0.8
     assert stations_level_over_threshold(stations, 0.8)[5][1] > 0.8
     assert stations_level_over_threshold(stations, 0.8)[20][1] > 0.8
+
+def test_stations_highest_rel_level():
+    stations = build_station_list()
+    update_water_levels(stations)
+    assert len(stations_highest_rel_level(stations, 10)) == 10
+    for n in range(0, N):
+        assert stations_highest_rel_level(stations, N)[n][1] > 1
