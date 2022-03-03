a74c3d62c326d14051599b1ac4fbc8363115f917
Waddell02 <lukewaddell16@gmail.com>
2022-03-03 05:06:47 +0000

    Error fix attempt

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -4,6 +4,12 @@ import matplotlib.dates
 import numpy as np
 from datetime import datetime, timedelta
 from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+from . station import MonitoringStation
+
+stations = build_station_list()
+update_water_levels(stations)
 
 def polyfit(dates, levels, p):
     ds = matplotlib.dates.date2num(dates)
@@ -19,15 +25,15 @@ def polyfit(dates, levels, p):
 
 def warning(stations):
     for station in stations:
-        if float(station.relative_water_level()) >= 10 or station.relative_water_level() <= -10:
+        if station.relative_water_level() >= 10 or station.relative_water_level() <= -10:
             continue
-        elif float(station.relative_water_level()) >= 3.0:
+        elif station.relative_water_level() >= 3.0:
             station.warning_level = 4
-        elif float(station.relative_water_level()) >= 2.5:
+        elif station.relative_water_level() >= 2.5:
             station.warning_level = 3
-        elif float(station.relative_water_level()) >= 2.0:
+        elif station.relative_water_level() >= 2.0:
             station.warning_level = 2
-        elif float(station.relative_water_level()) >= 1.5:
+        elif station.relative_water_level() >= 1.5:
             station.warning_level = 1
         else:
             station.warning_level = 0
